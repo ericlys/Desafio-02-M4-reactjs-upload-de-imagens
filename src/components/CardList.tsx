977dc3bef227081f5a65abe558a04359b30ec17f
Ericlys Moreira <ericlysm@gmail.com>
2022-05-14 13:24:04 -0300

    feat: Criacao do componente cardList

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -1,4 +1,12 @@
-import { SimpleGrid, useDisclosure } from '@chakra-ui/react';
+import {
+  Box,
+  Heading,
+  Image,
+  SimpleGrid,
+  Stack,
+  Text,
+  useDisclosure,
+} from '@chakra-ui/react';
 import { useState } from 'react';
 import { Card } from './Card';
 import { ModalViewImage } from './Modal/ViewImage';
@@ -17,14 +25,32 @@ interface CardsProps {
 
 export function CardList({ cards }: CardsProps): JSX.Element {
   // TODO MODAL USEDISCLOSURE
-
+  const { isOpen, onOpen, onClose } = useDisclosure();
   // TODO SELECTED IMAGE URL STATE
-
   // TODO FUNCTION HANDLE VIEW IMAGE
 
   return (
     <>
-      {/* TODO CARD GRID */}
+      <SimpleGrid columns={[1, 2, 3]} spacing={10}>
+        {cards &&
+          cards.map(card => (
+            <Box h="290" maxW="293.3px" background="#353431" borderRadius={6}>
+              <Image
+                borderRadius="6px 6px 0 0"
+                src={card.url}
+                objectFit="cover"
+                h={192.64}
+                w={293.3}
+              />
+              <Box pt={5} px={6}>
+                <Heading fontSize="2xl">{card.title}</Heading>
+                <Text mt={2} fontSize="lg">
+                  {card.description}
+                </Text>
+              </Box>
+            </Box>
+          ))}
+      </SimpleGrid>
 
       {/* TODO MODALVIEWIMAGE */}
     </>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,10 +9,11 @@ import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
 
 interface Image {
+  id: string;
   title: string;
   description: string;
   url: string;
-  ts: string;
+  ts: number;
   is: string;
 }
 
@@ -45,7 +46,7 @@ export default function Home(): JSX.Element {
   });
 
   const formattedData = useMemo(() => {
-    const formatted = data?.pages.map(d => d.data.flat());
+    const formatted = data?.pages.flatMap(d => d.data);
     return formatted;
   }, [data]);
 
@@ -62,7 +63,7 @@ export default function Home(): JSX.Element {
       <Header />
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
-        {/* <CardList cards={formattedData} /> */}
+        <CardList cards={formattedData} />
         {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
       </Box>
     </>
